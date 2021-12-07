6a824c49d68436703b1cc0dd04f2de365ac8c02d
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-12-07 08:59:23 -0500

    Finished skeleton of answer sheets

---
+++ answers/part3.sql
@@ -0,0 +1,14 @@
+#What is each pokemon's primary type?
+
+#What is Rufflet's secondary type?
+
+#What are the names of the pokemon that belong to the trainer with trainerID 303?
+    
+#How many pokemon have a secondary type Poison
+
+#What are all the primary types and how many pokemon have that type?
+
+#How many pokemon at level 100 does each trainer with at least one level 100 pokemone have? (Hint: your query should not display a trainer
+   
+#How many pokemon only belong to one trainer and no other?
+
---
+++ answers/part4.sql
@@ -0,0 +1,2 @@
+#Sort the data by finding out which trainer has the strongest pokemon so that this will act as a ranking of strongest to weakest trainer. You may interpret strongest in whatever way you want, but you will have to explain your decision.
+
